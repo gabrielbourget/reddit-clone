9eeee1f6a27bc58a5fdf00d81a351867efdbb86c
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-01 16:06:31 -0400

    css fix

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -10,7 +10,7 @@ const SignInPage = () => {
     <div className="absolute inset-0">
       <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
         <Link href="/" className={cn(
-          buttonVariants({ variant: "ghost" }), "self-start, -mt-20"
+          buttonVariants({ variant: "ghost" }), "self-start -mt-20"
         )}>
           <ChevronLeft className="mr-2 h-4 w-4" /> Home
         </Link>
