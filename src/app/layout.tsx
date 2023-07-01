47ddf1220ebd5177104ee134b5e8030152ab5dab
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-01 12:40:57 -0400

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
-import '@/styles/globals.css'
+import { Inter } from "next/font/google";
+import { cn } from '@/lib/utils';
+import '@/styles/globals.css';
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({subsets: ["latin"] });
 
 export const metadata = {
   title: 'Breadit',
@@ -11,8 +16,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en' className={cn(
+      'bg-white text-slate-900 antialiased light', inter.className
+    )}>
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
