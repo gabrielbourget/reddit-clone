3ebc89e8a19e7fead885534c3642e30821fd34e7
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-02 22:32:23 -0400

    Add session provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
 const Providers = ({ children }: { children: React.ReactNode }) => { 
   return (
     <QueryClientProvider client={queryClient}>
-      { children }
+      <SessionProvider>
+        {children}
+      </SessionProvider>
     </QueryClientProvider>
   )
 }
