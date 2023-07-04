c6bab79c6d5642ed039826a56c3244ac980797ad
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-04 18:11:05 -0400

    change username

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,23 +2,32 @@
 import { redirect } from "next/navigation";
 // -> Within codebase
 import { authOptions, getAuthSession } from "@/lib/auth"
-import UserNameForm from "@/components/UserNameForm";
+import UsernameForm from "@/components/UsernameForm";
 
+export const metadata = {
+  title: 'Settings',
+  description: 'Manage account and website settings.',
+}
 
 const page = async ({}) => {
   const session = await getAuthSession();
 
   if (!session?.user) { redirect(authOptions.pages?.signIn || "/sign-in" )}
 
-  
+
   return (
     <div className="max-w-4xl mx-auto py-12">
       <div className="grid items-start gap-8">
-        <h1 className="font-bold text-3xl md:text-4xl">Settings</h1>
+        <h1 className="font-bold text-3xl md:text-4xl mb-4">Settings</h1>
       </div>
 
       <div className="grid gap-10">
-        <UserNameForm />
+        <UsernameForm
+          user={{
+            id: session?.user.id,
+            username: session?.user.username || ""
+          }}
+        />
       </div>
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,9 @@ const Navbar = async () => {
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between">
         {/* Logo */}
-        <Icons.logo className="h-8 w-8 cm:h-6 sm:w-6" />
+        <Link href={"/"}>
+          <Icons.logo className="h-8 w-8 cm:h-6 sm:w-6" />
+        </Link>
         {/* <Link href="/" className="flex gap-2 items-center">
           <p className="hidden text-zinc-700 text-sm font-medium md:block">Breadit</p>
         </Link> */}
